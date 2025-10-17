c7333f13af467a8663961b528295d47a61b6bb34
Vinicius Cavalcanti de Freitas <vcf2@WINDOWS.CIN.UFPE.BR>
2025-10-17 18:39:41 -0300

    atualização historia pt2

--- historia.cpp
+++ historia.cpp
@@ -13,19 +13,52 @@ void historia::iniciar(){
     std:: string resp;
 
     std:: string ola = "Ola, viajante!";
-    std:: string pergunta = "Qual seu nome?";
+    std:: string frase1 = "eh a primeira vez que te vejo por aqui.";
+    std:: string pergunta = "pode me dizer qual eh o seu nome?";
+    std:: string pergunta1 = "";
 
+
+
+// as frases estao com delays, entao apliquei um for para aplicar um delay:
+
+// ola viajante ===============================
     for (char i : ola){
+        std::cout << i << std:: flush; // a frase esta numa variavel que vai rodar com um delay de 50 miliseconds
+        dalay(50); // isso acontece pra todo texto no bloco iniciar
+    }
+
+    dalay(1000); // isso aqui é uma pausa para a prox pagina
+    std:: cout << " " << std:: endl;
+// ===========================================
+
+// ===========================================
+
+    for (char i : frase1){
         std::cout << i << std:: flush;
-        dalay(100);
+        dalay(50);
     }
     dalay(1000);
     std:: cout << " " << std:: endl;
-    
-    for (char i : pergunta){
-        std::cout << i << std:: flush;
-        dalay(100);
-    }
+// ===========================================
 
+    while (resp != "1"){
+        for (char i : pergunta){
+            std::cout << i << std:: flush;
+            dalay(50);
+        }
+        std:: cout << " " << std:: endl;
+        std:: cout << "Nome: ";
+        std:: cin >> nome;
+
+        std:: cout << " " << std:: endl;
+        std:: cout << "Tem certeza desse nome? " << std:: endl;
+        std:: cout << " " << std:: endl;
+        std:: cout << "[ 1 ] sim " << std:: endl;
+        std:: cout << "[ 2 ] nao " << std:: endl;
+        std:: cout << " " << std:: endl;
+        std:: cin >> resp;
+        std:: cout << " " << std:: endl;
+
+    }
 
 }
--- toDO.md
+++ toDO.md
@@ -4,6 +4,8 @@ reformular jogador [x]
 
 reformular status [x]
 
+historia [ ]
+
 sistemas de habilidades (4 habilidades)[ ]
 
 sistemas de niveis [ ]
@@ -13,43 +15,3 @@ combate[ ]
 barries [ ]
 
 tipos de pokemon [ ]
-
-
-
-
-        std:: cout << "Qual seu nome?"<< std:: endl;
-        std:: cout << ""<< std:: endl;
-        std::cin >> nome; 
-        std:: cout << ""<< std:: endl;
-        std:: cout << "ola" << nome<< std:: endl;
-        std:: cout << ""<< std:: endl;
-        std:: cout << "esta pronto para a jornada?"<< std:: endl;
-        std:: cout << ""<< std:: endl;
-        std::cin >> resp; 
-        std:: cout << "Iniciar jornada!"<< std:: endl;
-
-
-            std:: cout << "O";
-    dalay(150);
-    std:: cout << "l";
-    dalay(100);
-    std:: cout << "a";
-    dalay(100);
-    std:: cout << " ";
-
-    std:: cout << "v";
-    dalay(100);
-    std:: cout << "i";
-    dalay(100);
-    std:: cout << "a";
-    dalay(100);
-    std:: cout << "j";
-    dalay(100);
-    std:: cout << "a";
-    dalay(100);
-    std:: cout << "n";
-    dalay(100);
-    std:: cout << "t";
-    dalay(100);
-    std:: cout << "e";
-    dalay(100);
